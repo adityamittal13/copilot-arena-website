8025bc2372dc04063b0d2c3bd78e7d61b88b80d6
Aditya <adityamittal307@gmail.com>
2024-10-29 01:07:58 -0400

    remove google-api-core requirement

--- backend/firebase_client.py
+++ backend/firebase_client.py
@@ -1,7 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-from google.cloud.firestore_v1 import aggregation
-from privacy import PrivacySetting, clean_data
 import pandas as pd
 
 
@@ -12,12 +10,6 @@ class FirebaseClient:
         firebase_admin.initialize_app(self.cred)
         self.db = firestore.client()
 
-    def upload_data(self, collection_name: str, data: dict, privacy: PrivacySetting):
-        collection_ref = self.db.collection(collection_name)
-        if privacy != PrivacySetting.RESEARCH:
-            data = clean_data(data)
-        collection_ref.add(data)
-
     def get_autocomplete_outcomes(self, collection_name: str, user_id: str = None):
         query = self.db.collection(collection_name)
         if user_id:
@@ -25,31 +17,3 @@ class FirebaseClient:
         outcomes = query.get()
         outcomes_df = pd.DataFrame([x.to_dict() for x in outcomes])
         return outcomes_df
-
-    def get_autocomplete_completions(
-        self, collection_name: str, models: list, user_id: str = None
-    ):
-        query = self.db.collection(collection_name)
-        filters = [firestore.FieldFilter("model", "in", models)]
-
-        if user_id:
-            filters.append(firestore.FieldFilter("userId", "==", user_id))
-
-        query = query.where(filter=firestore.And(filters))
-
-        completions = query.get()
-        completions_df = pd.DataFrame([x.to_dict() for x in completions])
-        return completions_df
-
-    def get_autocomplete_outcomes_count(
-        self, collection_name: str, user_id: str = None
-    ):
-        query = self.db.collection(collection_name)
-        if user_id:
-            query = query.where(filter=firestore.FieldFilter("userId", "==", user_id))
-
-        aggregate_query = aggregation.AggregationQuery(query)
-        aggregate_query.count(alias="all")
-
-        results = aggregate_query.get()
-        return results[0][0].value
