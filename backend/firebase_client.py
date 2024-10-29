e62fa955297065a3910568fcdb747ec294dda48e
Aditya <adityamittal307@gmail.com>
2024-10-29 01:09:57 -0400

    remove user_id check

--- backend/firebase_client.py
+++ backend/firebase_client.py
@@ -10,10 +10,8 @@ class FirebaseClient:
         firebase_admin.initialize_app(self.cred)
         self.db = firestore.client()
 
-    def get_autocomplete_outcomes(self, collection_name: str, user_id: str = None):
+    def get_autocomplete_outcomes(self, collection_name: str):
         query = self.db.collection(collection_name)
-        if user_id:
-            query = query.where(filter=firestore.FieldFilter("userId", "==", user_id))
         outcomes = query.get()
         outcomes_df = pd.DataFrame([x.to_dict() for x in outcomes])
         return outcomes_df
